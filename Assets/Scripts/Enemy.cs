cae3dfe0385b49d5516216e5ba7177911575fc5e
Yves Ruosch <yves@ruosch.me>
2021-02-10 21:34:59 +0100

    Step 104

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+    void Start()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+
+}
