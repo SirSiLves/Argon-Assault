4103741e611089f346f3e7c6b45c80e231c7a4a0
Yves Ruosch <yves@ruosch.me>
2021-02-10 18:45:25 +0100

    Step 97

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+
+}
